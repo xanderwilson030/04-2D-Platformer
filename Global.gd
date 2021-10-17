4c5c8542f1ff1313ffd5f5e6086a158552a995f3
Wilson, Xander Cohen <xcwilson@iu.edu>
2021-10-17 14:52:02 -0400

    Project Base

---
+++ Global.gd
@@ -0,0 +1,85 @@
+extends Node
+
+const SAVE_PATH = "user://savegame.sav"
+const SECRET = "C220 Is the Best!"
+var save_file = ConfigFile.new()
+
+onready var HUD = get_node_or_null("/root/Game/UI/HUD")
+onready var Coins = get_node_or_null("/root/Game/Coins")
+onready var Mines = get_node_or_null("/root/Game/Mines")
+onready var Game = load("res://Game.tscn")
+onready var Coin = load("res://Coin/Coin.tscn")
+onready var Mine = load("res://Mine/Mine.tscn")
+
+var save_data = {
+	"general": {
+		"score":0
+		,"health":100
+		,"coins":[]
+		,"mines":[]	
+	}
+}
+
+
+func _ready():
+	update_score(0)
+	update_health(0)
+
+func update_score(s):
+	save_data["general"]["score"] += s
+	HUD.find_node("Score").text = "Score: " + str(save_data["general"]["score"])
+
+func update_health(h):
+	save_data["general"]["health"] += h
+	HUD.find_node("Health").text = "Health: " + str(save_data["general"]["health"])
+
+func restart_level():
+	HUD = get_node_or_null("/root/Game/UI/HUD")
+	Coins = get_node_or_null("/root/Game/Coins")
+	Mines = get_node_or_null("/root/Game/Mines")
+	
+	for c in Coins.get_children():
+		c.queue_free()
+	for m in Mines.get_children():
+		m.queue_free()
+	for c in save_data["general"]["coins"]:
+		var coin = Coin.instance()
+		coin.position = str2var(c)
+		Coins.add_child(coin)
+	for m in save_data["general"]["mines"]:
+		var mine = Mine.instance()
+		mine.position = str2var(m)
+		Mines.add_child(mine)
+	update_score(0)
+	update_health(0)
+	get_tree().paused = false
+
+# ----------------------------------------------------------
+func save_game():
+	save_data["general"]["coins"] = []
+	save_data["general"]["mines"] = []
+	for c in Coins.get_children():
+		save_data["general"]["coins"].append(var2str(c.position))
+	for m in Mines.get_children():
+		save_data["general"]["mines"].append(var2str(m.position))
+
+	var save_game = File.new()
+	save_game.open_encrypted_with_pass(SAVE_PATH, File.WRITE, SECRET)
+	save_game.store_string(to_json(save_data))
+	save_game.close()
+	
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists(SAVE_PATH):
+		return
+	save_game.open_encrypted_with_pass(SAVE_PATH, File.READ, SECRET)
+	var contents = save_game.get_as_text()
+	var result_json = JSON.parse(contents)
+	if result_json.error == OK:
+		save_data = result_json.result
+	else:
+		print("Error: ", result_json.error)
+	save_game.close()
+	
+	var _scene = get_tree().change_scene_to(Game)
+	call_deferred("restart_level")
